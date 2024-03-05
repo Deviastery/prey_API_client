f0d31ae40c9360e6271b4254a28c09bd7e49f8e1
Viktoria T <deviastery@gmail.com>
2024-03-05 21:46:58 +0300

    refactoring(ResponseTextarea): refactoring function output

--- src/components/HomePage/ResponseTextarea/ResponseTextarea.jsx
+++ src/components/HomePage/ResponseTextarea/ResponseTextarea.jsx
@@ -13,25 +13,40 @@ const ResponseTextarea = ({ request }) => {
         if (request) {
 
             (async () => {
-                const res = await getResponse(request);
-                if (!(Array.isArray(res))) {
-                    let futureText = [];
-                    futureText.push(res);
-                    setText(futureText);
+                const prevRes = await getResponse(request);
+                console.log(prevRes);
+                if (prevRes) {
+                    if (!(Array.isArray(prevRes))) {
+                        let res = [];
+                        res.push(prevRes);
+                        setText(res);
+                        return;
+                    }
+                    setText(prevRes);
                     return;
                 }
-                setText(res);
+                setText('Not Found');
             })()
         }
-        }, [request]);
+    }, [request]);
+
+    function formatText(text) {
+        if (!text) return '';
+        if (text === 'Not Found') return 'Not Found';
+        return (text.map((item) => {
+            deleteTechnicalProperties(item);
+            return JSON.stringify(item, null, 4);
+        }).join(',\n'))
+    }
+
+    console.log(text);
 
     return (
         <>
             <textarea name="response" id="" cols="70" rows="30"
-                value={text && text.map((item) => {
-                    deleteTechnicalProperties(item);
-                    return JSON.stringify(item, null, 4);
-                }).join(',\n')}
+                value={
+                    formatText(text)
+                }
                 readOnly
             >
             </textarea>
